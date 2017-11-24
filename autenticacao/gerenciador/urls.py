f3774c235f89e26b3fcdd41391a8e7038cfce663
Gabriel <gabriel.lasso@usp.br>
2017-11-24 13:22:20 -0200

    Arrumei as URLs

--- autenticacao/gerenciador/urls.py
+++ autenticacao/gerenciador/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url(r'^$', gerenciador_views.home, name='home'),
+    url(r'^$', views.home, name='home'),
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^signup/$', views.signup, name='signup'),
