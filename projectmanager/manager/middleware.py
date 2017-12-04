3a21705aa79b3bb25939b8d8e601a763d4110723
angelolovatto <angelolovatto@gmail.com>
2017-12-04 21:19:24 -0200

    Renaming middleware variables
    Signed-off-by: angelolovatto <angelolovatto@gmail.com>

--- projectmanager/manager/middleware.py
+++ projectmanager/manager/middleware.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from django.contrib import messages
-from django.shortcuts import redirect
 from django.db import connection
 
 
 def queries_middleware(get_response):
     def middleware(request):
         response = get_response(request)
-        print("\n\nQUERIES")
+        print "\n\nPostgreSQL"
         print(connection.queries)
         return response
 
