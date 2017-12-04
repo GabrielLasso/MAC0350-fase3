3f9eca4af223a473c0bf4a8224946ac4a07f9e92
angelolovatto <angelolovatto@gmail.com>
2017-12-04 17:04:38 -0200

    middleware that shows sql queries in terminal
    Signed-off-by: angelolovatto <angelolovatto@gmail.com>

--- projectmanager/projectmanager/settings.py
+++ projectmanager/projectmanager/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'manager.middleware.queries_middleware',
 ]
 
 ROOT_URLCONF = 'projectmanager.urls'
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'projectmanager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
